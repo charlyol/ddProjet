5f94b45460b354ca302cb245a14266225cd60144
charlyol <147928338+charlyol@users.noreply.github.com>
2024-05-14 16:45:43 +0200

    first connection and first requete

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,8 +1,7 @@
 package Main;
 import Affichage.Menu;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,6 +12,21 @@ public class Main {
 
         try {
             Connection con = DriverManager.getConnection(url,userName,password);
+            Statement stmt = con.createStatement();
+
+            stmt.execute("insert into Heros(HeroClass, HeroName, HeroLife, HeroLifeMax, HeroLevelAtk , HeroLevelAtkMax,HeroPosition,HeroEquipementOffensif,HeroEquipementDeffensif) values ('Humain', 'Charly', '31','100', '10','30','1','Hand','foot')");
+
+
+
+            ResultSet resultset = stmt.executeQuery("select * from Heros");
+
+            while (resultset.next()) {
+                System.out.println(" id Hero : " + resultset.getInt("idHero"));
+                System.out.println(" Class Hero : " + resultset.getString("HeroClass"));
+                System.out.println(" Name Hero : " + resultset.getString("HeroName"));
+
+            }
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
