3db03ea90f2267a327a66ed78e181ae528b1764e
charlyol <147928338+charlyol@users.noreply.github.com>
2024-05-14 16:15:42 +0200

    connect to mysql localhost for db

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,22 @@
 package Main;
-
 import Affichage.Menu;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/GameD&D";
+        String userName="TotoDB";
+        String password="Totodu26&";
+
+        try {
+            Connection con = DriverManager.getConnection(url,userName,password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         Menu menu = new Menu();
         menu.affichage();
     }
